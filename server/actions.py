ec19a03aef6061e5b8672e56925b7ec027ba3be1
ldsvrn <louis2555@orange.fr>
2022-11-24 20:18:26 +0100

    actions base

--- server/actions.py
+++ server/actions.py
@@ -1,4 +1,22 @@
-def reboot():
-    pass
- 
-#...
+class Linux:
+    def __init__(self) -> None:
+        pass
+
+    def dostuff():
+        pass
+
+
+class Windows:
+    def __init__(self) -> None:
+        pass
+
+    def dostuff():
+        pass
+
+
+class OSX:
+    def __init__(self) -> None:
+        pass
+
+    def dostuff():
+        pass
