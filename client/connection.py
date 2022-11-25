55d4946ff1bd6d8108212117ac387238b27b3810
ldsvrn <louis2555@orange.fr>
2022-11-25 09:41:15 +0100

    working? client/server

--- client/connection.py
+++ client/connection.py
@@ -1,2 +1,42 @@
+#!/usr/bin/env python3
+import threading
+import socket
+import sys
+
+HOST = ("127.0.0.1", int(sys.argv[1]))
+
 class Connection:
-    pass
+    def __init__(self) -> None:
+        self.client = socket.socket()
+        try:
+            self.client.connect(HOST)
+        except PermissionError:
+            print(f"Permissions insuffisantes pour utiliser le port {HOST[1]}")
+            sys.exit(13) # EACCES Permission denied
+        except ConnectionRefusedError:
+            print(f"Impossible de ce connecter au serveur {HOST}")
+            sys.exit(118) # EHOSTUNREACH Host is unreachable
+        else:
+            client_handler = threading.Thread(target=self.handle, args=[self.client])
+            client_handler.start()
+
+            while True:
+                msgcl = input("Message:")
+                self.client.send(msgcl.encode())
+            self.client.close()
+
+    
+    def handle(self, conn) -> None:
+        while True:
+            msgsrv = conn.recv(1024)
+            if not msgsrv: break # prevents infinite loop on disconnect
+            msgsrv = msgsrv.decode()
+            print(f"\nMessage du serveur: {msgsrv}")
+            match msgsrv:
+                case "dostuff":
+                    print("didstuff")
+                    break
+        conn.close()
+
+if __name__ == "__main__":
+    conn= Connection()
--- server/main.py
+++ server/main.py
@@ -3,11 +3,12 @@
 import threading
 import socket
 import logging
+import sys
 
 logging.basicConfig(level=logging.DEBUG)
 
 # TODO: Detect IP?
-HOST = ("127.0.0.1", 1000)
+HOST = ("127.0.0.1", int(sys.argv[1]))
 
 
 class Server:
@@ -24,19 +25,22 @@ class Server:
 
             thread = threading.Thread(target=self.handle, args=(conn, addr))
             # keeping sockets and threads in a list of tuples
-            self.clients.append(zip(addr, conn, thread))
+            self.clients.append((addr, conn, thread))
 
             thread.start()
 
-    def handle(client: socket.socket, addr: tuple) -> None:
+    def handle(self, client: socket.socket, addr: tuple) -> None:
         # TODO: remove while true maybe
         while True:
             message = client.recv(1024)
+            if not message: break # prevents infinite loop on disconnect
+            message = message.decode()
             logging.info(f"Message from {addr}: {message}")
 
             match message:
                 case "dostuff":
-                    pass
+                    print("didstuff")
+                    client.send("didstuff".encode())
 
 
 if __name__ == "__main__":
