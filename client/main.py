5cd77ad615f64a5aee7276ff9b2c942b7013f81c
ldsvrn <louis2555@orange.fr>
2022-12-05 16:31:25 +0100

    open tab function

--- client/main.py
+++ client/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import sys
+import csv
+
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -16,6 +18,7 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QFont
+
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
@@ -48,43 +51,9 @@ class Tab(QWidget):
         self.tabs = []
         self.connections = []
         # Add a bunch of tabs
-        # FIXME: this should be a separate function to allow the creation of tabs by the user
-        for i in range(10):
-            self.tabs.append(
-                {
-                    "widget": QWidget(),
-                    "Button_info": QPushButton("Get info."),
-                    "Label_info": QLabel("Placeholder\nPlaceholder\nPlaceholder\nPlaceholder\nPlaceholder"),
-                    "ComboBox_shell": QComboBox(),
-                    "LineEdit_sendcommand": QLineEdit("Send a command..."),
-                    "Button_sendcommand": QPushButton("Send"),
-                    "LineEdit_resultcommand": QPlainTextEdit("""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-culpa qui officia deserunt mollit anim id est laborum.
-                    """)
-                }
-            )
-            self.tabwidget.addTab(self.tabs[i]["widget"], f"Tab {i}")
         
-            # Set the layout
-            self.tabs[i]["widget"].layout = QGridLayout()
-            self.tabs[i]["widget"].setLayout(self.tabs[i]["widget"].layout)
-
-            ### FIRST COL
-            self.tabs[i]["widget"].layout.addWidget(self.tabs[i]["Button_info"], 0, 0)
-            self.tabs[i]["widget"].layout.addWidget(self.tabs[i]["Label_info"], 1, 0, 3, 1)
-
-            ### SECOND COL
-            self.tabs[i]["widget"].layout.addWidget(self.tabs[i]["ComboBox_shell"], 0, 1)
-            self.tabs[i]["widget"].layout.addWidget(self.tabs[i]["LineEdit_sendcommand"], 0, 2)
-            self.tabs[i]["widget"].layout.addWidget(self.tabs[i]["Button_sendcommand"], 0, 3)
-            self.tabs[i]["widget"].layout.addWidget(self.tabs[i]["LineEdit_resultcommand"], 1, 1, 3, 3)
-
-            self.tabs[i]["LineEdit_resultcommand"].setFont(self.monospace)
+        for i in range(10):
+            self._create_tab(str(i))
 
         LineEdit_addr = QLineEdit("IP")
         LineEdit_port = QLineEdit("Port")
@@ -94,6 +63,53 @@ culpa qui officia deserunt mollit anim id est laborum.
         self.layout.addWidget(LineEdit_port, 0, 1)
         self.layout.addWidget(Button_conn, 0, 2)
         self.layout.addWidget(self.tabwidget, 1, 0, 1, 3)
+
+    def _create_tab(self, name: str):
+        self.tabs.append(
+            {
+                "widget": QWidget(),
+                "Button_info": QPushButton("Get info."),
+                "Label_info": QLabel(
+                    "Placeholder\nPlaceholder\nPlaceholder\nPlaceholder\nPlaceholder"
+                ),
+                "ComboBox_shell": QComboBox(),
+                "LineEdit_sendcommand": QLineEdit("Send a command..."),
+                "Button_sendcommand": QPushButton("Send"),
+                "LineEdit_resultcommand": QPlainTextEdit(
+                    """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.
+                """
+                ),
+            }
+        )
+        self.tabwidget.addTab(self.tabs[-1]["widget"], name)
+
+        # Set the layout
+        self.tabs[-1]["widget"].layout = QGridLayout()
+        self.tabs[-1]["widget"].setLayout(self.tabs[-1]["widget"].layout)
+
+        ### FIRST COL
+        self.tabs[-1]["widget"].layout.addWidget(self.tabs[-1]["Button_info"], 0, 0)
+        self.tabs[-1]["widget"].layout.addWidget(self.tabs[-1]["Label_info"], 1, 0, 3, 1)
+
+        ### SECOND COL
+        self.tabs[-1]["widget"].layout.addWidget(self.tabs[-1]["ComboBox_shell"], 0, 1)
+        self.tabs[-1]["widget"].layout.addWidget(
+            self.tabs[-1]["LineEdit_sendcommand"], 0, 2
+        )
+        self.tabs[-1]["widget"].layout.addWidget(
+            self.tabs[-1]["Button_sendcommand"], 0, 3
+        )
+        self.tabs[-1]["widget"].layout.addWidget(
+            self.tabs[-1]["LineEdit_resultcommand"], 1, 1, 3, 3
+        )
+
+        self.tabs[-1]["LineEdit_resultcommand"].setFont(self.monospace)
 
 
 if __name__ == "__main__":
