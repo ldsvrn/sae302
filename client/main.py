998567aa42a2412247c66c7cc4dc338ba7237a38
ldsvrn <louis2555@orange.fr>
2022-12-05 17:40:34 +0100

    user now able to open tabs, proper closing handling

--- client/main.py
+++ client/main.py
@@ -17,17 +17,36 @@ from PyQt5.QtWidgets import (
     QPlainTextEdit,
 )
 from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QCloseEvent
 import connection
+import logging
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("SAE3.02")
         self.setMinimumSize(1000, 500)
-        self.setCentralWidget(Tab(self))
+        self.tab = Tab(self)
+        self.setCentralWidget(self.tab)
         self.show()
 
+    def closeEvent(self, _e: QCloseEvent):
+        if self.tab.tabs_open >= 1:
+            box = QMessageBox()
+            box.setWindowTitle("Quit ?")
+            box.setText("Do you really wish to quit? This will disconnect you from all servers.")
+            box.addButton(QMessageBox.Yes)
+            box.addButton(QMessageBox.No)
+
+            ret = box.exec()
+
+            if ret == QMessageBox.Yes:
+                self.tab.disconnect_all()
+                QCoreApplication.exit(0)
+            else:
+                _e.ignore()
+        else:
+            QCoreApplication.exit(0)
 
 """
 https://pythonspot.com/pyqt5-tabs/
@@ -52,29 +71,33 @@ class Tab(QWidget):
         self.servers = []
         # Add a bunch of tabs
 
-        # TODO: try except 
-        with open("servers.csv", "r") as csvfile:
-            for row in csv.reader(csvfile):
-                self.servers.append(
-                    {
-                        "name": str(row[0]),
-                        "ip": str(row[1]),
-                        "port": int(row[2])
-                    }
-                )
-        
-        print(self.servers)
+        # FIXME: should be relative to the script location
+        try:
+            with open("servers.csv", "r") as csvfile:
+                for row in csv.reader(csvfile):
+                    self.servers.append(
+                        {
+                            "name": str(row[0]),
+                            "ip": str(row[1]),
+                            "port": int(row[2])
+                        }
+                    )
+        except Exception as e:
+            logging.error(f"Error loading servers.csv, ignoring... ({e})")
+            pass
+        else:
+            for conn in self.servers:
+                self._create_tab(conn["name"], conn["ip"], conn["port"])
 
-        for conn in self.servers:
-            self._create_tab(conn["name"], conn["ip"], conn["port"])
+        self.LineEdit_addr = QLineEdit("IP")
+        self.LineEdit_port = QLineEdit("Port")
+        self.Button_conn = QPushButton("Connect")
 
-        LineEdit_addr = QLineEdit("IP")
-        LineEdit_port = QLineEdit("Port")
-        Button_conn = QPushButton("Connect")
+        self.Button_conn.clicked.connect(self._connect_Clicked)
 
-        self.layout.addWidget(LineEdit_addr, 0, 0)
-        self.layout.addWidget(LineEdit_port, 0, 1)
-        self.layout.addWidget(Button_conn, 0, 2)
+        self.layout.addWidget(self.LineEdit_addr, 0, 0)
+        self.layout.addWidget(self.LineEdit_port, 0, 1)
+        self.layout.addWidget(self.Button_conn, 0, 2)
         self.layout.addWidget(self.tabwidget, 1, 0, 1, 3)
 
     def _create_tab(self, name: str, ip: str, port: int):
@@ -102,6 +125,7 @@ culpa qui officia deserunt mollit anim id est laborum.
         )
         tab = self.tabs[-1]
 
+        # TODO: URGENT: try except here or the programme WILL crash if a wrong ip is given
         tab["conn"] = connection.Connection(ip, port, tab["Label_info"], tab["LineEdit_resultcommand"])
 
         self.tabwidget.addTab(tab["widget"], name)
@@ -122,6 +146,20 @@ culpa qui officia deserunt mollit anim id est laborum.
 
         tab["LineEdit_resultcommand"].setFont(self.monospace)
 
+    def _connect_Clicked(self):
+        ip = self.LineEdit_addr.text()
+        port = int(self.LineEdit_port.text())
+        self._create_tab(ip, ip, port)
+    
+    def disconnect_all(self):
+        if len(self.tabs) > 0:
+            for i in self.tabs:
+                i["conn"].disconnect()
+    
+    @property
+    def tabs_open(self) -> int:
+        return len(self.tabs)
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
